7d160952230b21b0ae2e025afb53987ede334b76
tsolmon khorloo <tsolmonkhorloo@tsolmons-MacBook-Pro.local>
2024-02-14 23:26:38 +0800

    fix: bash migrate command

--- scripts/00-symfony-deploy.sh
+++ scripts/00-symfony-deploy.sh
@@ -4,3 +4,22 @@ composer install --no-dev --optimize-autoloader
 
 echo "Caching config..."
 APP_ENV=prod APP_DEBUG=0 php bin/console cache:clear
+
+# Attempt database connection in a loop with exponential backoff
+attempt=1
+max_attempts=5  # Adjust as needed, based on expected connection retries
+wait_time=1      # Adjust as needed, based on retry intervals
+
+if [ "$1" = 'php-fpm' ] || [ "$1" = 'bin/console' ]; then
+	until bin/console doctrine:query:sql "select 1" >/dev/null 2>&1; do
+	    (>&2 echo "Waiting for DB to be ready...")
+		sleep 1
+	done
+
+	echo "Migrating..."
+    bin/console doctrine:migrations:migrate --no-interaction
+    echo "Database migrations completed successfully."
+fi
+
+# Further application or script actions here
+echo "Application ready!"
