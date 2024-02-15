912086120a3a0bcd329a6f62ad32795f40a5ec1a
tsolmon khorloo <tsolmonkhorloo@tsolmons-MacBook-Pro.local>
2024-02-15 18:21:08 +0800

    fix: jwt token command

--- src/Entity/Quiz.php
+++ src/Entity/Quiz.php
@@ -2,7 +2,6 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Metadata\ApiProperty;
 use App\Repository\QuizRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
